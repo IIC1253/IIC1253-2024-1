b72ed24d14f4f34881b4c3bcbc93f868e2278013
Camila Carrasco <cicarrasco5@uc.cl>
2024-03-27 21:05:26 -0300

    t2

---
+++ Tareas/t2.py
@@ -0,0 +1,10 @@
+def is_valid_expression(s):
+    # aquí va su desarrollo
+    pass
+
+# aquí pueden definir funciones auxiliares
+
+if __name__ == "__main__":
+    # ejecución de ejemplos
+    print(is_valid_expression('((3-(4*((4+1)*8)))/7)'))
+    print(is_valid_expression('((3-(4*((4+1)*8))/7)'))
